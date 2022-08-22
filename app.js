d29f27736ee7427ebb6503758b15911638962870
inna <innazozulia@outlook.com>
2022-08-22 14:59:55 +0300

    async deleting files, and folders

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 fs.readFile("./text.txt", (error, data) => {
   fs.mkdir("./files", () => {
     fs.writeFile(
-      "./files/test.txt",
+      "./files/text.txt",
       `${data} this new text exapmle two`,
       (error) => {
         error ? console.log(error) : null;
@@ -11,3 +11,16 @@ fs.readFile("./text.txt", (error, data) => {
     );
   });
 });
+
+// delete files and folders
+setTimeout(() => {
+  if (fs.existsSync("./files/text.txt")) {
+    fs.unlink("./files/text.txt", () => {});
+  }
+}, 4000);
+
+setTimeout(() => {
+  if (fs.existsSync("./files")) {
+    fs.rmdir("./files", () => {});
+  }
+}, 6000);
